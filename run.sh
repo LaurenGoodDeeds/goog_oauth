5271c5132f0fe1159cb6a634658e49559dc8c82a
Lauren <lauren@givegooddeeds.com>
2021-01-14 16:06:25 -0600

    Teeny app for oAuth login w the Googs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
 export FN_BASE_URI=http://localhost:8040
-export FN_CLIENT_ID=1064822893553-gap02pmgq0ripjrjj56dgsuhug9613tt.apps.googleusercontent.com
-export FN_CLIENT_SECRET=LdZ5yM64DgBZTwB08Meo85xB
+export FN_CLIENT_ID=<client_id>.apps.googleusercontent.com
+export FN_CLIENT_SECRET=<client_secret>
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
