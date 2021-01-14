02c57df9bac03b6f667be9def704ec70955a38ac
Lauren <lauren@givegooddeeds.com>
2021-01-14 16:03:35 -0600

    Teeny app for oAuth login w the Googs

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+import os
+import flask
+import google_auth
+
+app = flask.Flask(__name__)
+app.secret_key = os.environ.get("FN_FLASK_SECRET_KEY", default=False)
+
+app.register_blueprint(google_auth.app)
+
+@app.route('/')
+def index():
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+        return '<div>You are currently logged in as ' + user_info['given_name'] + ' with ' + user_info['email'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+
+    return 'You aint logged in fool.'
---
+++ run.sh
@@ -0,0 +1,10 @@
+export FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
+export FN_BASE_URI=http://localhost:8040
+export FN_CLIENT_ID=1064822893553-gap02pmgq0ripjrjj56dgsuhug9613tt.apps.googleusercontent.com
+export FN_CLIENT_SECRET=LdZ5yM64DgBZTwB08Meo85xB
+
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+export FN_FLASK_SECRET_KEY=BLAHBLAHBLAH
+
+python -m flask run -p 8040
